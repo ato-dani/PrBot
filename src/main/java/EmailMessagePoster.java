b0606d928059ced303159f0cea50468c7f8ca26d
vi <mason.beldon@beldon.com>
2021-11-02 19:20:40 -0400

    More error checking for EmailMessagePoster

--- src/main/java/EmailMessagePoster.java
+++ src/main/java/EmailMessagePoster.java
@@ -51,6 +51,12 @@ public class EmailMessagePoster implements MessagePoster{
     }
 
     public ResponseFormatter postMessage(AccessTokenInfo accessTokenInfo, String title, String message, String channel){
+        if(!validateEmailsSet(emailTo)){
+            return new ResponseFormatter(false, "Failed to send email, invalid email TO address!");
+        }
+        else if(!validateEmails(emailFrom)){
+            return new ResponseFormatter(false, "Failed to send email, invalid email FROM address!");
+        }
         try{
             if(sendEmail(title, message)){
                 return new ResponseFormatter(true, "Successfully sent email");
@@ -108,4 +114,24 @@ public class EmailMessagePoster implements MessagePoster{
         }
         return true;
     }
+
+    public static boolean validateEmailsSet(Set<String> emails){
+        //taken from RFC 5322, regex for email validation.
+        //see https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b" +
+                "\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0" +
+                "-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9]" +
+                "[0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[" +
+                "\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m;
+        for (String email : emails) {
+            m = pattern.matcher(email);
+            if(!m.matches()){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
