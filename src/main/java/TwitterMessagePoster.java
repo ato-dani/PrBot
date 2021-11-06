0b5c54bc09c10d85adb4eff9df377012267cdc91
vi <mason.beldon@beldon.com>
2021-11-06 15:13:58 -0400

    Twitter works now (for real this time). Doesn't attempt to log you in twice

--- src/main/java/TwitterMessagePoster.java
+++ src/main/java/TwitterMessagePoster.java
@@ -9,10 +9,10 @@ public class TwitterMessagePoster implements MessagePoster{
     AccessTokenInfo loginToken = null;
     TwitterFactory twitterFactory = null;
 
-    public TwitterMessagePoster(AccessTokenInfo devToken){
+    public TwitterMessagePoster(AccessTokenInfo loginToken){
         try{
-            loginToken = OAuthHandler.authorizeTwitter(devToken.getAccessToken(), devToken.getAccessTokenSecret());
-            this.devToken = devToken;
+            this.loginToken = loginToken;
+            devToken = new AccessTokenInfo(System.getenv("TWITTER_CLIENT_ID"), System.getenv("TWITTER_CLIENT_SECRET"), null);
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setOAuthConsumerKey(devToken.getAccessToken())
                     .setOAuthConsumerSecret(devToken.getAccessTokenSecret())
